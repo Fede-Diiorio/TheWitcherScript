3199ceeb10eadc2af9c900a4793dc3aeae98f8d1
Federico Di Iorio <federico_metallica@hotmail.com>
2024-12-30 06:04:31 -0300

    Se crearon todas las tablas requeridas en la BBDD

--- tables.sql
+++ tables.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE IF NOT EXISTS bestiary;
+USE bestiary;
+
 CREATE TABLE categories (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR (30) NOT NULL
@@ -17,4 +20,35 @@ CREATE TABLE monsters (
     hability VARCHAR (150),
     FOREIGN KEY (category) REFERENCES categories (id) ON DELETE CASCADE,
     FOREIGN KEY (location) REFERENCES locations (id) ON DELETE CASCADE
+);
+
+CREATE TABLE loots (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR (100) NOT NULL
+);
+
+CREATE TABLE weaknesses_type (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR (15) NOT NULL
+);
+
+CREATE TABLE weaknesses (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR (75) NOT NULL,
+    type INT NOT NULL,
+    FOREIGN KEY (type) REFERENCES weaknesses_type (id) ON DELETE CASCADE
+);
+
+CREATE TABLE monsters_weaknesses (
+    id_monster INT NOT NULL,
+    id_weakness,
+    FOREIGN KEY (id_monster) REFERENCES monsters (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_weakness) REFERENCES weaknesses (id) ON DELETE CASCADE
+);
+
+CREATE TABLE monsters_loots (
+    id_monster INT NOT NULL,
+    id_loot,
+    FOREIGN KEY (id_monster) REFERENCES monsters (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_loot) REFERENCES loots (id) ON DELETE CASCADE
 );
