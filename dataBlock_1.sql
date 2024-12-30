480b46f7215c5a68913a212b9bcc104d972010ac
Federico Di Iorio <federico_metallica@hotmail.com>
2024-12-30 06:43:53 -0300

    Se incorporaron selects y algunos datos

---
+++ dataBlock_1.sql
@@ -0,0 +1,8 @@
+INSERT INTO weaknesses_type (name) VALUES
+('Aceite'), ('Señal'), ('Bomba');
+
+INSERT INTO categories (name) VALUES
+('Bestia'), ('Maldito'), ('Dracónido'),
+('Constructo'), ('Híbrido'), ('Insectoide'),
+('Necrófago'), ('Ogroide'), ('Relicto'),
+('Espectro'), ('Vampiro');
